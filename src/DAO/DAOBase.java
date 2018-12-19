4109275f77eb9fa237cdc0f889cad1168377aaae
田小龟 <@LAPTOP-JTORTTRB>
2018-12-19 10:45:47 +0800

    DAoBase

---
+++ src/DAO/DAOBase.java
@@ -0,0 +1,13 @@
+package DAO;
+
+import java.sql.Connection;
+
+public class DAOBase implements DAO {
+
+	@Override
+	public Connection getConnection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
