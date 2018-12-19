146765459edb883477419aabd221940f4b1cab4b
161002229litiantian <1020416882@qq.com>
2018-12-19 19:28:39 +0800

    update

--- src/DAO/KindBookDAO.java
+++ src/DAO/KindBookDAO.java
@@ -6,6 +6,6 @@ import Bean.kindbook;
 
 public interface KindBookDAO {
 
-	public List<kindbook> searchByC(String callnumber);//
+	public List<kindbook> searchByC(String callnumber);//²é³ö
 	public void updateInfo(kindbook k);
 }
