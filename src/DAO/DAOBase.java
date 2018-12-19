a2f00a6c76174cbf0c1689e5d464e3b3f5446f35
BiBaBoooo <954578336@qq.com>
2018-12-19 16:28:35 +0800

    complete DAOBase

--- src/DAO/DAOBase.java
+++ src/DAO/DAOBase.java
@@ -1,15 +1,26 @@
 package DAO;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DAOBase implements DAO {
 	
 	@Override
 	public Connection getConnection() {
 		Connection conn=null;
-		System.out.println("");
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			String url="jdbc:mysql://localhost:3306/librarydb";
+			String user="root";
+			String password="gwy19980811";
+			conn=DriverManager.getConnection(url, user, password);
+		}catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
 	}
 
 }
