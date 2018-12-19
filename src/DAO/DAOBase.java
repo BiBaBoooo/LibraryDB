d416cc3751f7c2c9c72202f7d42d90698df7e016
田小龟 <@LAPTOP-JTORTTRB>
2018-12-19 11:07:45 +0800

    DAOBase2

--- src/DAO/DAOBase.java
+++ src/DAO/DAOBase.java
@@ -7,7 +7,7 @@ public class DAOBase implements DAO {
 	@Override
 	public Connection getConnection() {
 		Connection conn=null;
-		System.out.println();
+		System.out.println("");
 		// TODO Auto-generated method stub
 		return null;
 	}
