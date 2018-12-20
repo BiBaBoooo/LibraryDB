cfc08ec34091fbef4566efa7b24958dbdc3a57b6
BiBaBoooo <954578336@qq.com>
2018-12-20 21:38:51 +0800

    update interface

--- src/DAO/SepcificBookDAO.java
+++ src/DAO/SepcificBookDAO.java
@@ -7,6 +7,6 @@ import Bean.sepcificbook;
 public interface SepcificBookDAO {
 
 	public List<sepcificbook> search(String bookname);	//如果bookname为null,则查询所有的书；若不为null,则之查询对应的书
-	public List<sepcificbook> lookup(String barcode);		//根据索书号查询  
+	public List<sepcificbook> lookup(String barcode);		//根据条形码查询  
 	public boolean updatesecificbook(String place,String  barcode);//更新一本书的位置信息
 }
