02b4801c4fff6edbce378c9737b96e258388b73a
BiBaBoooo <954578336@qq.com>
2018-12-20 21:38:39 +0800

    modify method

--- src/DAO/SDAOConcrete.java
+++ src/DAO/SDAOConcrete.java
@@ -6,8 +6,6 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-
-import Bean.kindbook;
 import Bean.sepcificbook;
 
 public class SDAOConcrete extends DAOBase implements SepcificBookDAO {
