0a73d557088eebf5d6c28f67c016f03a0dc70c0a
BiBaBoooo <954578336@qq.com>
2018-12-20 21:39:29 +0800

    add method

--- src/DAO/UDAOConcrete.java
+++ src/DAO/UDAOConcrete.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import Bean.UserInfo;
 import Bean.user;
 
 public class UDAOConcrete extends DAOBase implements UserDAO {
@@ -21,11 +22,11 @@ public class UDAOConcrete extends DAOBase implements UserDAO {
 			String sql="select * from user where useris=?";
 			ps=conn.prepareStatement(sql);
 			ps.setString(1, userid);
+			rs=ps.getResultSet();
 			if(rs.next()) {
 				u=new user();
 				u.setUserid(userid);
 				u.setPassword(rs.getString("password"));
-				u.setMaxborrow(rs.getInt("maxborrow"));
 			}
 			rs.close();
 			ps.close();
@@ -37,7 +38,7 @@ public class UDAOConcrete extends DAOBase implements UserDAO {
 	}
 
 	@Override
-	public user getUser(String userid, String pwd) {
+	public user getUserByID(String userid, String pwd) {
 		Connection conn = null;
 		PreparedStatement ps=null;
 		user user1=null;
@@ -49,9 +50,40 @@ public class UDAOConcrete extends DAOBase implements UserDAO {
 			ps = conn.prepareStatement(finalsql);
 			ps.setString(1, userid);
 			ps.setString(2, pwd);
+			rs=ps.getResultSet();
 			if(rs.next()) {
 				user1=new user();
 				user1.setUserid(userid);
+				user1.setPassword(pwd);			
+			}
+			rs.close();
+			ps.close();
+			conn.close();
+
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return user1;
+	}
+
+	@Override
+	public user getUserByEM(String email, String pwd) {
+		Connection conn = null;
+		PreparedStatement ps=null;
+		user user1=null;
+		ResultSet rs=null;
+		try{
+			conn = getConnection();
+			String finalsql = null;
+			finalsql="select * from user where email=? and password=? ";
+			ps = conn.prepareStatement(finalsql);
+			ps.setString(1, email);
+			ps.setString(2, pwd);
+			rs=ps.getResultSet();
+			if(rs.next()) {
+				user1=new user();
+				user1.setUserid(rs.getString("userid"));
+				user1.setUserid(email);
 				user1.setPassword(pwd);
 				
 			}
@@ -63,6 +95,52 @@ public class UDAOConcrete extends DAOBase implements UserDAO {
 			e.printStackTrace();
 		}
 		return user1;
+	}
+
+	@Override
+	public UserInfo queryUser(String userid) {
+		
+		Connection conn = null;
+		PreparedStatement ps=null;
+		ResultSet rs=null;
+		UserInfo u=new UserInfo();
+		
+		try {
+			conn=getConnection();
+			String sql1="select * from userdetail where userid=?";
+			ps=conn.prepareStatement(sql1);
+			ps.setString(1, userid);
+			rs=ps.getResultSet();
+			if(rs.next()) {
+				u.setUserid(userid);
+				u.setName(rs.getString("name"));
+				u.setMaxborrow(rs.getInt("maxborrow"));
+				u.setBorrowcount(rs.getInt("borrowcount"));
+			}
+			rs.close();
+			ps.close();
+			
+			String sql2="select * from user where userid=?";
+			ps=conn.prepareStatement(sql2);
+			ps.setString(1, userid);
+			rs=ps.getResultSet();
+			if(rs.next()) {
+				u.setEmail(rs.getString("email"));
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				rs.close();
+				ps.close();
+				conn.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return u;
 	}
 
 }
