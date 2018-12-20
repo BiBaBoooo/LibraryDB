356612169f584b39adde3a8e635f79a851531e84
161002229litiantian <1020416882@qq.com>
2018-12-20 14:42:27 +0800

    add a new bean KS.java

---
+++ src/Bean/KS.java
@@ -0,0 +1,68 @@
+package Bean;
+
+import java.util.Date;
+
+public class KS {
+
+	private String callnumber;	//索书号
+	private String bookname;	//书名
+	private String author;	//作者
+	private int count;	//相同的书在图书馆中的数量
+	
+	private String barcode;		//条形码
+	private Date buydate;		//购书时间
+	private String place;		//藏书位置
+	private String state;		//是否被借
+	
+	
+	public String getCallnumber() {
+		return callnumber;
+	}
+	public void setCallnumber(String callnumber) {
+		this.callnumber = callnumber;
+	}
+	public String getBookname() {
+		return bookname;
+	}
+	public void setBookname(String bookname) {
+		this.bookname = bookname;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	public String getBarcode() {
+		return barcode;
+	}
+	public void setBarcode(String barcode) {
+		this.barcode = barcode;
+	}
+	public Date getBuydate() {
+		return buydate;
+	}
+	public void setBuydate(Date buydate) {
+		this.buydate = buydate;
+	}
+	public String getPlace() {
+		return place;
+	}
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	
+}
