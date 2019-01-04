6b56ca2b58cae527a9e4a6029645fcf850d51bb2
Tianzw <1441185165@qq.com>
2019-01-04 20:18:25 +0800

    kc

--- src/Bean/kc.java
+++ src/Bean/kc.java
@@ -1,17 +1,17 @@
 package Bean;
 
 public class kc {
+	public int getcID() {
+		return cID;
+	}
+	public void setcID(int cID) {
+		this.cID = cID;
+	}
 	public int getId() {
 		return id;
 	}
 	public void setId(int id) {
 		this.id = id;
-	}
-	public String getcID() {
-		return cID;
-	}
-	public void setcID(String cID) {
-		this.cID = cID;
 	}
 	public String getCallnumber() {
 		return callnumber;
@@ -20,6 +20,6 @@ public class kc {
 		this.callnumber = callnumber;
 	}
 	private int id;	//相同的书在图书馆中的数量
-	private String cID;		//书本简介
+	private int cID;		//书本简介
 	private String callnumber;
 }
